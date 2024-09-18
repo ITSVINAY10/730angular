f12bd7daa2a1aba7246e2da7bd4eba562e96abd2
ITSVINAY10 <itsvinayvelagana7@gmail.com>
2024-09-18 09:09:14 +0530

    upgrade

---
+++ angular/code/directives/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+
+
+
+
+  day:number = 5;
+  fruits = ["apple","mango","banana","papaya"]
+}
