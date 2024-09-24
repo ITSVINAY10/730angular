52a4002ef6c34b12cdd3ddc2b0f3d8a31b65e0cf
ITSVINAY10 <itsvinayvelagana7@gmail.com>
2024-09-24 07:29:48 +0530

    latest updates

---
+++ angular/code/nestedComponents/src/app/movies/movies.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrl: './movies.component.css'
+})
+export class MoviesComponent {
+  tilluURL = 'https://tse3.mm.bing.net/th?id=OIP.8Rd9Y6ZvQVt2ZXQazvfNtgAAAA&pid=Api&P=0&h=220';
+  kalkiURL = 'https://tse3.mm.bing.net/th?id=OIP.50_ptGIH3_0pqDCGrIH__gHaEK&pid=Api&P=0&h=220';
+  devaraURL = 'https://imagesv2.desimartini.com/images/202406/jr-ntr-in-devara-1718279902.jpeg';
+  minioneURL = 'https://tse4.mm.bing.net/th?id=OIP.4NI15bdNSqicewDFKvU-5AHaEK&pid=Api&P=0&h=220';
+  KGFURL = 'https://tse3.mm.bing.net/th?id=OIP.zGPwviGW6LCK1HYQkQDqzgHaKl&pid=Api&P=0&h=220'
+}
