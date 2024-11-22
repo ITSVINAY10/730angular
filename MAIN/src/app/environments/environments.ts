2c690208421fbbdb2107ce53e232da34a8351322
ITSVINAY10 <itsvinayvelagana7@gmail.com>
2024-11-22 10:47:22 +0530

    project completed

---
+++ MAIN/src/app/environments/environments.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTtYC6-HoBWpHn96eLMdgM9k_-IiVSs3Y",
+  authDomain: "angdemo-c4196.firebaseapp.com",
+  projectId: "angdemo-c4196",
+  storageBucket: "angdemo-c4196.appspot.com",
+  messagingSenderId: "871242052474",
+  appId: "1:871242052474:web:3a8577f371b3d5fe88dcc3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
